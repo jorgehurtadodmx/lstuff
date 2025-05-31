680d80dbbbf2c4cd75bad413146aa027ab197432
Jorge Hurtado moxó <32094091+jorgehurtadodmx@users.noreply.github.com>
2025-05-31 10:46:01 +0200

    test commit george

--- src/main/java/com/grupo1/Main.java
+++ src/main/java/com/grupo1/Main.java
@@ -8,7 +8,7 @@ import org.springframework.context.ApplicationContext;
 public class Main {
 
 	public static void main(String[] args) {
-
+		System.out.println("test");
 		ApplicationContext spring = SpringApplication.run(Main.class, args);
 
 
