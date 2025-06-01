b4c4ac525683301732de36fb25d3e3a1c15667ef
Jorge Hurtado moxó <32094091+jorgehurtadodmx@users.noreply.github.com>
2025-06-01 15:16:08 +0200

    user detail

--- src/main/java/com/grupo1/controllers/UsuarioController.java
+++ src/main/java/com/grupo1/controllers/UsuarioController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usuarios")
@@ -31,4 +33,31 @@ public class UsuarioController {
         model.addAttribute("usuarios", usuarios);
         return "user-list";
     }
+
+
+    @GetMapping("/{id}")
+    public String findById(Model model, @PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if (usuario.isPresent()) {
+            model.addAttribute("usuario", usuario.get());
+
+        } else {
+            model.addAttribute("error", "404 usuario no encontrado");
+        }
+        return "user-detail";
+    }
+
+    @GetMapping("/{id}/editar")
+        public String editar(Model model, @PathVariable Long id)  {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if (usuario.isPresent()) {
+            model.addAttribute("usuario", usuario.get());
+            //resto de datos relacionados con usuario para pasarselos a la vista.
+            //model.addAttribute("proyectos", proyectoRepository.findAll());
+        } else {
+            model.addAttribute("error", "usuario no encontrado");
+        }
+        return "/user-detail-edit";
+    }
+
 }
