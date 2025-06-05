778739fafc8089849d5ab9556767de3d584e8972
Javier Noguer Fernández <jnoguerfernandez@gmail.com>
2025-06-05 18:22:22 +0200

    modificado ProjectController

--- src/main/java/com/grupo1/controllers/ProjectController.java
+++ src/main/java/com/grupo1/controllers/ProjectController.java
@@ -1,4 +1,26 @@
 package com.grupo1.controllers;
 
+import com.grupo1.entities.Project;
+import com.grupo1.repositories.ProjectRepository;
+import com.grupo1.repositories.TaskRepository;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/products")
 public class ProjectController {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public ProjectController(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+    
 }
