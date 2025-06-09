78ffc00813a43b582bce19cd3b6350f08fc7eae0
Javier Noguer Fernández <jnoguerfernandez@gmail.com>
2025-06-10 01:01:10 +0200

    modificado ProjectController

--- src/main/java/com/grupo1/repositories/ProjectRepository.java
+++ src/main/java/com/grupo1/repositories/ProjectRepository.java
@@ -3,9 +3,12 @@ package com.grupo1.repositories;
 import com.grupo1.entities.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     Project findByNombreIgnoreCase(String nombre);
 
+    List<Project> findByNombreContainsIgnoreCase(String nombre);
 
 }
